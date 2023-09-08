4e47827d0be520b936e1a933701097207f2dbc8e
[Achim Terra] <[aedbuilt24@gmail.com]>
2023-09-08 13:00:53 +0100

    nineteen commits

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const navToggle = document.querySelector('.bars');
+const topBtn = document.getElementById('back-to-top-button');
+
+
+navToggle.addEventListener('click', function() {
+    links.classList.toggle('show-links');
+});
